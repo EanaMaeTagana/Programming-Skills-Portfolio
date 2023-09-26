acd9933fb3fe67db3b7cf38218ba5d47451e93a5
EanaMaeTagana <144775623+EanaMaeTagana@users.noreply.github.com>
2023-09-26 12:39:53 +0400

    Delete Chapter 1 directory

--- Chapter 1/Exercise 1-5.py
+++
@@ -1,8 +0,0 @@
-"""
-Exercise 1-5: Compute Area of Circle
-"""
-# Write a Python program which accepts the radius of a circle from the user and compute the area.
-
-from math import pi
-r=float(input("Input the radius of the circle:"))
-print("The area of the circle with the given radius is:",pi*r**2)
