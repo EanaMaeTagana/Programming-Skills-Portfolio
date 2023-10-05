00438dd58b2594fed5698132ead42fdad6e9c3fa
EanaMaeTagana <144775623+EanaMaeTagana@users.noreply.github.com>
2023-10-05 16:24:11 +0400

    Delete Chapter 1 directory

--- Chapter 1/Exercise 1-3.py
+++
@@ -1,9 +0,0 @@
-"""
-Chapter 1, Exercise 3: Print Date and Time
-"""
-# Write a Python program to display the current date and time
-
-import datetime
-now = datetime.datetime.now()
-print ("The current date and time is:")
-print (now.strftime("%Y-%m-%d %H:%M:%S"))
