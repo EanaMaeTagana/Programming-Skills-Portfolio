ef5ed9cff4bd6424703ad2b7624fad8ba0cf2ed2
EanaMaeTagana <144775623+EanaMaeTagana@users.noreply.github.com>
2023-10-26 10:24:01 +0400

    Delete Chapter 2 directory

--- Chapter 2/Exercise 2-2.py
+++
@@ -1,6 +0,0 @@
-"""
-Chapter 2, Exercise 2: Variables
-"""
-# Find a quote from a famous person you admire. Print the quote and the name of its author.
-
-print('Taylor Swift once said, "I wanna be defined by the things that I love. I just think that you are what you love."')
